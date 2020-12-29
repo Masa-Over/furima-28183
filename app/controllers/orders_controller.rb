d647398b5d91f67489cce2e3cd63db4c0ed51b3a
Masa-Over <69909857+Masa-Over@users.noreply.github.com>
2020-12-29 21:53:24 +0900

    商品購入機能　STEP2　修正2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,8 @@
 class OrdersController < ApplicationController
 
   def index
-    # @orders = Order.all
+    @user_order = 
   end
 
-  def new
-    @order = Order.new
-  end
 
-  def create
-    @order = Order.new(order_params)
-     if @order.valid?
-       @order.save
-       redirect_to 
-     else
-       render root_path
-     end
-  end
-
-  private
-  def order_params
-    params.reqiure(:order).permit(:prefecture_id, :municipalities, :address, :postal_code, :phone_number, :building, :user, :item).merge(user_id: current_user_id, item_id: params[:tweet_id])
-  end
 end
--- db/migrate/20201229091419_create_street_addresses.rb
+++ db/migrate/20201229091419_create_street_addresses.rb
@@ -1,6 +1,13 @@
 class CreateStreetAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :street_addresses do |t|
+      t.integer :prefecture_id, null: false
+      t.string :municipalities, null: false
+      t.string :address, null: false
+      t.string :postal_code, null: false
+      t.string :phone_number, null: false
+      t.string :building 
+      t.references :purchase_record, foreign_key:true
 
       t.timestamps
     end
