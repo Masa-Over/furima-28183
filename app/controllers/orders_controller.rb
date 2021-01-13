d29dbc1b4397a90e504a3e085f2dd729c062f1a0
Masa-Over <69909857+Masa-Over@users.noreply.github.com>
2021-01-13 19:03:21 +0900

    商品購入機能　STEP4

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,5 +4,18 @@ class OrdersController < ApplicationController
     @user_order = UserOrder.new
   end
 
+  def create
+    @user_order = UserOrder.new(order_params)
+    if @user_order.valid?
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:user_order).permit(:prefecture_id, :municipalities, :address, :postal_code, :phone_number, :building )
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,16 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  with_options presence: true do
+    validates :name, length: {maximum: 40}
+    validates :explanation, length: {maximum: 1000}
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9999999}
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :delivery_fee_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :send_day_id, numericality: { other_than: 1 }
+    validates :image
+  end
 
   belongs_to :user
   has_one :purchase_record
